b41d2bad64c7cfd388ef28ef8cc7a4368ef6b025
Himel Majumder <himelmajumder1@gmail.com>
2024-02-10 20:14:59 +0100

    Added factories and migrations for categories and posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        \App\Models\Post::factory(100)->create();
+        \App\Models\Category::factory(5)->create();
+
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+
     }
 }
