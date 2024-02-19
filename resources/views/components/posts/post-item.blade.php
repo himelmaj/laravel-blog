ec331f044a8bf76ed23d1aa62d5c013dd890efcf
Himel Majumder <himelmajumder1@gmail.com>
2024-02-19 22:21:21 +0100

    Utilize Blade's {!! !!} directive to render the post excerpt without escaping HTML. This ensures that any HTML markup included in the excerpt is properly displayed.

--- resources/views/components/posts/post-item.blade.php
+++ resources/views/components/posts/post-item.blade.php
@@ -3,28 +3,30 @@
 <article class="[&:not(:last-child)]:border-b border-gray-100 pb-10">
     <div class="article-body grid grid-cols-12 gap-3 mt-5 items-start">
         <div class="article-thumbnail col-span-4 flex items-center">
-            <a href="/blog/{{$post->slug}}">
-                <img class="mw-100 mx-auto rounded-xl" src="{{$post->getImagePathAttribute()}}" alt="{{$post->title}}">
+            <a href="/blog/{{ $post->slug }}">
+                <img class="mw-100 mx-auto rounded-xl" src="{{ $post->getImagePathAttribute() }}"
+                    alt="{{ $post->title }}">
             </a>
         </div>
         <div class="col-span-8">
             <div class="article-meta flex py-1 text-sm items-center">
-                <img class="w-7 h-7 rounded-full mr-3" src="{{$post->author->profile_photo_url}}" alt="Avatar {{$post->author->name}}">
-                <span class="mr-1 text-xs">{{$post->author->name}}</span>
-                <span class="text-gray-500 text-xs">{{$post->published_at->diffForHumans()}}</span>
+                <img class="w-7 h-7 rounded-full mr-3" src="{{ $post->author->profile_photo_url }}"
+                    alt="Avatar {{ $post->author->name }}">
+                <span class="mr-1 text-xs">{{ $post->author->name }}</span>
+                <span class="text-gray-500 text-xs">{{ $post->published_at->diffForHumans() }}</span>
             </div>
             <h2 class="text-xl font-bold text-gray-900">
                 <a href="">
-                    {{$post->title}}
+                    {{ $post->title }}
                 </a>
             </h2>
 
             <p class="mt-2 text-base text-gray-700">
-                {{$post->getExcerptAttribute()}}
+                {!! $post->getExcerptAttribute() !!}
             </p>
             <div class="article-actions-bar mt-6 flex items-center justify-between">
                 <div class="flex items-center space-x-4">
-                    <span class="text-gray-500 text-sm">{{$post->getReadingTime()}} min read</span>
+                    <span class="text-gray-500 text-sm">{{ $post->getReadingTime() }} min read</span>
                 </div>
                 {{-- <div>
                     <a class="flex items-center">
